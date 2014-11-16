61008c307106dd20e6767293e9b836321af33faa
adqin <41262633@qq.com>
2014-11-16 17:33:54 +0800

    update

---
+++ application/config/config.inc.php
@@ -0,0 +1,18 @@
+<?php
+return array(
+    'site_url' => 'http://niwenwen.com',
+    'site_name' => '你问问',
+    'site_note' => '常常问自己, 想要些什么？',
+    'upload_url' => 'http://up.niwenwen.com',
+    
+    // 数据库配置
+    'db_user' => 'root',
+    'db_pwd' => 'adqin1001',
+    'db_name' => 'wecms',
+    'db_charset' => 'utf8',
+    
+    // 路由规则
+    'router' => array(
+        
+    ),
+);
